f918efa0c819f7235e48dbb49af0b715462fd2b2
mindofsimon <simone5.mariani@mail.polimi.it>
2020-09-21 10:56:03 +0200

    flute and music signal function

---
+++ music_file.m
@@ -0,0 +1,31 @@
+function music_freqs = music_file(file_name, win_length ,n_frames)
+    [y,fs]=audioread(file_name);
+    S = zeros(n_frames, fs/2);
+    k = zeros(n_frames, 1);
+    for m = 0:n_frames-1
+    
+        % signal windowing 
+        y_m = y(m*win_length +1 : m*win_length + win_length/2).*hamming(win_length)';
+
+        % compute the FFT
+        Y = fft(y_m,fs);
+
+        % retain only the positive frequencies up to the Nyquist index
+        Y = Y(1:fs/2);
+
+        % populate S with the magnitude of the m-th spectrum
+        S(m+1,:) = abs(Y);        
+    
+    end
+
+    % magnitude to dB
+    S_dB = 20*log10(S);
+
+    % find the indices of the spectral maxima
+    k_b = zeros(n_frames,1);
+    for m = 0 : n_frames-1
+        [~,k_b(m+1)] = max(S_dB(m+1 , : ));
+    end
+    music_freqs=k_b;
+end
+
--- vocoder.m
+++ vocoder.m
@@ -8,17 +8,17 @@ duration_wind = 0.02;
 D = duration_file/duration_wind; % ratio between file and 20ms window
 N = length(y);
 L = N / D ; % number of window samples   
-window = rectwin(L);
+window = hamming(L);
 
 
 i = 0; j = 0;
-correlations = zeros(D, 2*L-1);
+correlations = zeros(2*D, 2*L-1);
 excerpt = zeros(L,1);
 while (i+L <= N)
     excerpt(1:L) = (y(i+1:i+L));
     r = xcorr(excerpt);
     %[peaks, locs] = findpeaks(r);
-    i = i+ L; 
+    i = i+ L/2; 
     
     j = j+1;
     
@@ -28,8 +28,8 @@ end
 
 % Estimate LPC coefficients using Levinson's method
 lpc_order = 18; % order of LPC filter, we'll have lpc_order + 1 coefficients
-coefs = zeros(D, lpc_order + 1);
-for i = 1:D
+coefs = zeros(2*D, lpc_order + 1);
+for i = 1:2*D
     autocorrVec = correlations(i,1:lpc_order+1);
     err(1) = autocorrVec(1);
     k(1) = 0;
@@ -44,39 +44,43 @@ for i = 1:D
     coefs(i,:) = [1; A]';
 end
 
-msg=midi();%receiving a MIDI message (5 seconds)
+% msg=midi();%receiving a MIDI message (5 seconds)
+% 
+ gain=5;
+% 
+ freqs=zeros(1,2*D);
+% time=0;
+% m=2;
+% for i=1:2*D
+%     if(m<=length(msg))
+%         if (msg(m).Timestamp-time-msg(m-1).Timestamp<0.01)
+%             m=m+1;
+%             time=0;
+%         else
+%             time=time+0.01;
+%         end
+%     else
+%         m=length(msg);
+%     end
+%     if(msg(m-1).Velocity>0)
+%         freqs(i)=440 * 2^((msg(m-1).Note-69)/12);
+%     else
+%         %note off
+%         freqs(i)=30000;
+%     end
+% end
+% 
 
-gain=5;
-
-freqs=zeros(1,D);
-time=0;
-m=2;
-for i=1:D
-    if(m<=length(msg))
-        if (msg(m).Timestamp-time-msg(m-1).Timestamp<0.01)
-            m=m+1;
-            time=0;
-        else
-            time=time+0.01;
-        end
-    else
-        m=length(msg);
-    end
-    if(msg(m-1).Velocity>0)
-        freqs(i)=440 * 2^((msg(m-1).Note-69)/12);
-    else
-        %note off
-        freqs(i)=30000;
-    end
-end
+freqs = music_file('flute.wav', L, 2*D);
 
 excitation_signal=zeros(1,N);
 output=zeros(1,N);
 win=1;
-for i=1: L :N-L
+for i=1: L/2 :N-L
     excitation_signal(i:i+L-1)=treno(L,freqs(win));
     output(i:i+L-1)=filter(gain,coefs(win,:),excitation_signal(i:i+L-1));
     win=win+1;
 end
-
+ 
 audiowrite('output.wav',cast(output,'uint8'),Fs);
+
