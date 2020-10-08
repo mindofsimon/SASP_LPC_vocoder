2dd02283ff63458415ec345afd25503ea8d5e072
mindofsimon <simone5.mariani@mail.polimi.it>
2020-10-08 16:22:49 +0200

    added I/O file sounds to interface

--- start_vocoder.m
+++ start_vocoder.m
@@ -1,5 +1,7 @@
 function [] = start_vocoder(app)
-    [x, fs] = audioread('prova_vocoder_44_2.wav');
+    input_file='prova_vocoder_44_2.wav';
+    [x, fs] = audioread(input_file);
+    app.InputFileEditField.Value=input_file;
     
     y = lpc_vocoder_main(x,fs,app);
 end
