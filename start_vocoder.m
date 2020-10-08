ae9376f8c9c5e39d8f17780b39d65fcb2db053bc
mindofsimon <simone5.mariani@mail.polimi.it>
2020-10-08 16:14:01 +0200

    converted starting script into a function

--- start_vocoder.m
+++ start_vocoder.m
@@ -1,7 +1,6 @@
-close all;
-
-[x, fs] = audioread('prova_vocoder_44_2.wav');
-
-y = lpc_vocoder_main(x,fs,interface);
-
+function [] = start_vocoder(app)
+    [x, fs] = audioread('prova_vocoder_44_2.wav');
+    
+    y = lpc_vocoder_main(x,fs,app);
+end
 
--- start_vocoder_2.m
+++
@@ -1,6 +0,0 @@
-function [] = start_vocoder_2(app)
-    [x, fs] = audioread('prova_vocoder_44_2.wav');
-    
-    y = lpc_vocoder_main(x,fs,app);
-end
-
