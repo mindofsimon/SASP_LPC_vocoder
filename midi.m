62e4ee37971ebfd8a21ced143ace450db7a6e0f4
mindofsimon <simone5.mariani@mail.polimi.it>
2020-10-28 18:26:40 +0100

    fixed midi device issue

--- midi.m
+++ midi.m
@@ -1,6 +1,10 @@
 function [msg] = midi()
-    device = mididevice('MPK Mini Mk II');
+    device = app.MIDIDropDown.Value;
     %display('Start Playing');
+    
+    %MIDI FUNCTION START
+        %here
+        
     h1=helpdlg('Press OK to start recording','Record');%open dialog win
     uiwait(h1);%wait for confirmation
     pause(6);%5 for a little latency
@@ -8,5 +12,8 @@ function [msg] = midi()
     h2=helpdlg('Finished Recording','Record');%open dialog win
     uiwait(h2);%wait for confirmation
     msg = midireceive(device);
+    
+    %MIDI FUNCTION STOP
+        %here
 end
 
